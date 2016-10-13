e6aa67ca3c3602f4a7331defe0deb57e978f118f
Zack McCauley <zackmccauley@TECH-Zack-McCauley.local>
2016-10-13 15:56:44 -0600

    Added better model handling. Uses actual ppd *NickName per cups PPD Format. Should be more stable and reliable

--- profile_generator.py
+++ profile_generator.py
@@ -5,6 +5,7 @@ Generates Printer Profile
 # Libraries
 import os
 import argparse
+import gzip
 from plistlib import writePlist
 from uuid import uuid4
 
@@ -13,20 +14,58 @@ profileuuid = str(uuid4())
 payloaduuid = str(uuid4())
 _options = {}
 
-
 # Parser Options
-parser = argparse.ArgumentParser(description='Generate a Configuration Profile for printer installation.')
-parser.add_argument('--printername', help='Name of printer queue. May not contain spaces, tabs, # or /. Required.', required=True)
-parser.add_argument('--driver', help='Name of driver file in /Library/Printers/PPDs/Contents/Resources/. Can be relative or full path. Required.',required=True)
-parser.add_argument('--address', help='IP or DNS address of printer. If no protocol is specified, defaults to socket://. Required.',required=True)
-parser.add_argument('--location', help='Location name for printer. Optional. Defaults to printername.')
-parser.add_argument('--displayname', help='Display name for printer (and Munki pkginfo). Optional. Defaults to printername.')
-parser.add_argument('--version', help='Version number of Munki pkginfo. Optional. Defaults to 1.0.', default='1.0')
-parser.add_argument('--organization', help='Change Organization of Profile. Defaults to GitHub', default="GitHub")
-parser.add_argument('--identifier', help='Change Profile + Payload Identifier before uuid. Payload UUID is appended to ensure it is unique. Defaults to com.github.wardsparadox', default="com.github.wardsparadox")
-parser.add_argument('--option', help='Add an option in addition to the "printer is shared" option. Bool values must be in True or False form.',action='append')
+parser = argparse.ArgumentParser(
+    description='Generate a Configuration Profile for printer installation.')
+parser.add_argument(
+    '--printername',
+    help='Name of printer queue. May not contain spaces, tabs,'
+    '# or /. Required.',
+    required=True)
+parser.add_argument(
+    '--driver',
+    help='Name of driver file in'
+    '/Library/Printers/PPDs/Contents/Resources/.'
+    'Can be relative or full path. Required.',
+    required=True)
+parser.add_argument(
+    '--address',
+    help='IP or DNS address of printer.'
+    'If no protocol is specified, defaults to socket://.'
+    'Required.',
+    required=True)
+parser.add_argument(
+    '--location',
+    help='Location name for printer.'
+    'Optional. Defaults to printername.')
+parser.add_argument(
+    '--displayname',
+    help='Display name for printer (and Munki pkginfo).'
+    'Optional. Defaults to printername.')
+parser.add_argument(
+    '--version',
+    help='Version number of Munki pkginfo.'
+    'Optional. Defaults to 1.0.',
+    default='1.0')
+parser.add_argument(
+    '--organization',
+    help='Change Organization of Profile.'
+    'Defaults to GitHub',
+    default="GitHub")
+parser.add_argument(
+    '--identifier',
+    help='Change Profile + Payload Identifier before uuid.'
+    'Payload UUID is appended to ensure it is unique.'
+    'Defaults to com.github.wardsparadox',
+    default="com.github.wardsparadox")
+parser.add_argument(
+    '--option',
+    help='Add an option in addition to the "printer is shared" option.'
+    'Bool values must be in True or False form.',
+    action='append')
 # Removed CSV for now.
-#parser.add_argument('--csv', help='Path to CSV file containing printer info. If CSV is provided, all other options are ignored.')
+#parser.add_argument('--csv', help='Path to CSV file containing printer info.
+# If CSV is provided, all other options are ignored.')
 
 # Main
 args = parser.parse_args()
@@ -49,11 +88,20 @@ else:
 if args.driver.startswith('/Library'):
     # Assume the user passed in a full path rather than a relative filename
     driver = args.driver
-    model = driver.rsplit('/', 1)[1].split('.', 1)[0]
+    with gzip.open(driver, 'rb') as ppd:
+        for line in ppd:
+            if "*NickName: " in line:
+                model = line.split("\"")[1]
+        ppd.close()
 else:
     # Assume only a relative filename
-    driver = os.path.join('/Library/Printers/PPDs/Contents/Resources', args.driver)
-    model = args.driver.split('.', 1)[0]
+    driver = os.path.join('/Library/Printers/PPDs/Contents/Resources',
+                          args.driver)
+    with gzip.open(driver, 'rb') as ppd:
+        for line in ppd:
+            if "*NickName: " in line:
+                model = line.split("\"")[1]
+            ppd.close()
 
 if '://' in args.address:
     # Assume the user passed in a full address and protocol
@@ -69,9 +117,10 @@ else:
     profileidentifier = args.identifier
     payloadidentifier = "{0}.{1}".format(args.identifier, payloaduuid)
 
-for option in args.option:
-    item = option.split('=')
-    _options[item[0]]=item[1]
+if args.option:
+    for option in args.option:
+        item = option.split('=')
+        _options[item[0]] = item[1]
 
 _options["printer-is-shared"] = False
 
@@ -87,7 +136,6 @@ _printer["PrinterLocked"] = False
 _printer["Option"] = _options
 Printer[args.printername] = _printer
 
-
 # Payload Content
 _payload = {}
 _payload["PayloadDisplayName"] = "Printing"
@@ -100,7 +148,8 @@ _payload["UserPrinterList"] = Printer
 
 # Profile info
 _profile = {}
-_profile["PayloadDisplayName"] = "{0} Printer Profile {1}".format(displayName, version)
+_profile["PayloadDisplayName"] = "{0} Printer Profile {1}".format(displayName,
+                                                                  version)
 _profile["PayloadIdentifier"] = profileidentifier
 _profile["PayloadOrganization"] = args.organization
 _profile["PayloadRemovalDisallowed"] = False
@@ -113,4 +162,5 @@ _profile["PayloadContent"] = [_payload]
 # Complete Profile
 Profile = _profile
 
-writePlist(Profile, "AddPrinter_{0}_{1}.mobileconfig".format(args.printername, version))
+writePlist(Profile, "AddPrinter_{0}_{1}.mobileconfig".format(args.printername,
+                                                             version))
